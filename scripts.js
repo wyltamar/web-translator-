5a957b41c1ed9870d5e041165f06cc2157d3c2ec
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-01-18 19:00:56 -0300

    created scrips.js file

---
+++ scripts.js
@@ -0,0 +1,20 @@
+
+let inputTextArea = document.querySelector(".input-text-area");
+
+async function translateText() {
+    
+   
+   let address = "https://api.mymemory.translated.net/get?q=" + inputTextArea.value
+                     + "&langpair=pt|en";
+
+    let  response =  await fetch(address)
+
+    let data = await response.json();
+
+    console.log(data);
+    
+    let translatedText = data.responseData.translatedText;
+    console.log(translatedText);
+
+}
+
