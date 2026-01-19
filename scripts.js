c841100e6765d64c498e8c0a9ed8c28b635ab786
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-01-19 16:00:38 -0300

    Informing the language  that is being dynamically  translated

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 
 let inputTextArea = document.querySelector(".input-text-area"); /* Seleciona a área de texto de entrada */
 let translatedText = document.querySelector(".translation"); /* Seleciona o elemento onde o texto traduzido será exibido */
+let languageSelect = document.querySelector(".language-select"); /* Seleciona o elemento de seleção de idioma */
 
 async function translateText() { 
     
    
    let address = "https://api.mymemory.translated.net/get?q=" + inputTextArea.value
-                     + "&langpair=pt|en"; /* Define o endereço da API de tradução com o texto de 
+                     + "&langpair=pt|"
+                     + languageSelect.value; /* Define o endereço da API de tradução com o texto de 
                                                 entrada e os idiomas */
 
     let  response =  await fetch(address) /* Faz uma requisição para a API de tradução */
